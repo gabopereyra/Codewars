fb9fcec9de4f25f299b12cf7eec999bc36fc7121
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-06-17 18:18:19 -0300

    gitignore uptaded + fixed description on class

--- src/kyu7/LengthAndTwoValues.java
+++ src/kyu7/LengthAndTwoValues.java
@@ -4,9 +4,9 @@
 /**
  * DESCRIPTION
  * Given an integer n and two other values, build an array of size n filled with these two values alternating.
- * <p>
+ *
  * Examples
- * <p>
+ *
  * 5, true, false     -->  [true, false, true, false, true]
  * 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
  * 0, "one", "two"    -->  []
